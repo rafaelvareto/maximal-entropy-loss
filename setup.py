afc15f281414d7498537c70e5a9360ac7a6ff868
Rafael Vareto <rafaelvareto@dcc.ufmg.br>
2024-01-15 16:00:47 -0300

    New version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="openloss",
-    version="0.0.1",
+    version="0.0.2",
     description="Cost functions designed for open-set classification tasks, published in well-known peer-reviewed venues.",
     packages=find_packages(where="python"),
     package_dir={"" : "python"},
