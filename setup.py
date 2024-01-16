1d188a77567abfc5c1f63845efe9f1941ba072b2
Rafael Vareto <rafaelvareto@dcc.ufmg.br>
2024-01-15 22:09:04 -0300

    Versioning commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="openloss",
-    version="0.0.1",
+    version="0.0.1.1",
     description="Cost functions designed for open-set classification tasks, namely, Entropic Open-set, ObjectoSphere and Maximal-Entropy Loss.",
     packages=find_packages(where="python"),
     package_dir={"" : "python"},
